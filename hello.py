5ad4fd2689bb2485223b2d6f27a4e09f663f9e4d
Mark Mwangi <mwangimark113@gmail.com>
2026-03-01 15:32:04 +0000

    first test code

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+future_age = age + 5
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+print(f"In 5 years, you will be {future_age}.")
+print("Time moves, whether we notice or not.")
